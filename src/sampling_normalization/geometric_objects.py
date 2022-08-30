d43fad8a1f43a9521bbdc39779b02b16a88c7e73
AllaVinner <joel.w.ottosson@gmail.com>
2022-08-30 20:36:11 +0200

    Setup abstract class for geometric objects

--- src/sampling_normalization/geometric_objects.py
+++ src/sampling_normalization/geometric_objects.py
@@ -1,9 +1,26 @@
 import numpy as np
 
-def spiral(t, initial_angle=0, angular_freq=2*np.pi):
-    x = t*np.cos(t*angular_freq+initial_angle)
-    y = t*np.sin(t*angular_freq+initial_angle)
-    return np.stack([x,y], axis=-1)   
+
+from abc import ABC, abstractmethod
+
+class Geometric(ABC):
+
+    @abstractmethod
+    def __call__(self, t, *args, **kwargs):
+        pass
+
+
+class Spiral(Geometric):
+
+    def __init__(self, initial_angle:float=0., angular_frequency:float=2*np.pi):
+        self.initial_angle = initial_angle
+        self.angular_frequency = angular_frequency
+
+    def __call__(self, t, *args, **kwargs):
+        angle = self.angular_freq + self.initial_angle
+        x = t*np.cos(t*angle)
+        y = t*np.sin(t*angle)
+        return np.stack([x,y], axis=-1)   
 
     
 
