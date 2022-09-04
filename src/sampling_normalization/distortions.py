bf40d35ee76e69ea467ac52f8aab41c17a148531
AllaVinner <joel.w.ottosson@gmail.com>
2022-09-04 18:47:55 +0200

    Refactoring Destortions

--- src/sampling_normalization/distortions.py
+++ src/sampling_normalization/distortions.py
@@ -1,11 +1,18 @@
 import numpy as np
 from sklearn.preprocessing import normalize
 
+
 def constant_radial_distortion(samples, distortion_std:float=0.01):
     unit_samples = normalize(samples)
     distortions = np.random.normal(scale=distortion_std, size=len(samples))
     return samples+np.diag(distortions)@unit_samples
 
+
+def radial_distortion(samples, distortion_std:float=0.01):
+    distortions = np.random.gamma(1/distortion_std**2, distortion_std**2, len(samples))
+    print('Distortions: ', distortions)
+    distortions = np.random.normal(scale=distortion_std, size=len(samples))
+    return np.diag(distortions)@samples
 
 
 
