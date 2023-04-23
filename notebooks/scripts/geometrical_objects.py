22280883b221881ce5cea93aab4b1a5df177f0f2
AllaVinner <joel.w.ottosson@gmail.com>
2023-04-23 18:48:52 +0200

    Added scripts for normalized spiral

--- notebooks/scripts/geometrical_objects.py
+++ notebooks/scripts/geometrical_objects.py
@@ -30,7 +30,7 @@ class Spiral():
         return self(t)
 
 
-def NormalizedSpiral():
+class NormalizedSpiral():
 
     def __init__(self, initial_angle:float=0., angular_frequency:float=2*np.pi, t_max:float=2*np.pi, *args, **kwargs):
         self.initial_angle = initial_angle
@@ -40,8 +40,8 @@ def NormalizedSpiral():
         self.domain = [0, self.t_max]
 
     def __call__(self, t, *args, **kwargs):
-        inv_value(self.length_at, target_value=self.length*t/self.t_max,
-                  min_guess=0, max_guess=self.t_max)
+        t = inv_value(self.length_at, target_value=self.length*t/self.t_max,
+                     min_guess=0, max_guess=self.t_max)
         x = t*np.cos(t*self.angular_frequency + self.initial_angle)
         y = t*np.sin(t*self.angular_frequency + self.initial_angle)
         return np.stack([x,y], axis=-1)
@@ -49,6 +49,8 @@ def NormalizedSpiral():
     def in_domain(self, t):
         return self.domain[0] <= t <= self.domain[1]
 
+    def length_at(self, t):
+        return 1/2*t*(np.sqrt(1+t*t))+1/2*np.log(t+np.sqrt(1+t*t))
 
 
 class Disk():
